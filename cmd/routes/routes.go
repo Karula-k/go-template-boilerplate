e4fb668bd1b2a30b7cbdd5d057a7a2d9897529a7
Karula-k <d.fikriirsyad@gmail.com>
2025-05-12 23:15:06 +0700

    add middleware to router

--- cmd/routes/routes.go
+++ cmd/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/go-template-boilerplate/cmd/controllers"
+	"github.com/go-template-boilerplate/cmd/middlewares"
 	"github.com/go-template-boilerplate/generated"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
@@ -14,4 +15,5 @@ func Routes(app *fiber.App, ctx context.Context, queries *generated.Queries) {
 	app.Post("/auth/register", controllers.RegisterController(ctx, queries))
 	app.Post("/auth/refresh_token", controllers.RefreshToken(ctx, queries))
 	app.Get("/swagger/*", swagger.HandlerDefault)
+	app.Use(middlewares.MiddleWare())
 }
